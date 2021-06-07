3d326bba1e6a0e306156b4d4b0cdd5e44a5313d8
shahryarkhan95 <58082896+shahryarkhan95@users.noreply.github.com>
2021-06-07 22:04:50 +0500

    OK New Code

--- gmaps_123/lib/homepage.dart
+++ gmaps_123/lib/homepage.dart
@@ -46,73 +46,56 @@ class HomePageState extends State<HomePage> {
           _googleMap(context),
           // _zoomminusfunction(),
           // _zoomplusfunction(),
-          _buildContainer( ),
+          _buildContainer(),
         ],
       ),
-
- 
-
     );
-
   }
 
 // CONTAINER FOR LIST VIEW
 
-Widget _buildContainer() {  
-  return Align(
-    alignment: Alignment.bottomLeft, 
-    child: Container(
-      margin: EdgeInsets.symmetric(vertical:20.0),
-      height:150.0,
-      
-      //LIST VIEW ADDED HERE
-      child:ListView(
-        scrollDirection:Axis.horizontal,
-        children:<Widget>[
-              SizedBox(width:10.0),
+  Widget _buildContainer() {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 150.0,
 
-              //FITTED BOX
- 
- //BOX 1
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child:_boxes (
-                  "assets/swim.jpg" , 
-                  31.4756, 74.3424, "Lahore 1 Swimming Pool"
-                  ),
-                ),
-              
-            SizedBox(width:10.0),
+        //LIST VIEW ADDED HERE
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            SizedBox(width: 10.0),
+
+            //FITTED BOX
+
+            //BOX 1
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes("assets/swim.jpg", 31.4756, 74.3424,
+                  "Lahore 1 Swimming Pool"),
+            ),
+
+            SizedBox(width: 10.0),
 //BOX 2
-               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child:_boxes (
-                  "assets/swim2.jpg" , 
-                  31.4756, 74.3424, "Lahore 2 Swimming Pool"
-                  ),
-                ),
-             
-            SizedBox(width:10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes("assets/swim2.jpg", 31.4756, 74.3424,
+                  "Lahore 2 Swimming Pool"),
+            ),
+
+            SizedBox(width: 10.0),
 //BOX 3
-               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child:_boxes (
-                  "assets/swim3.jpg" , 
-                  31.4756, 74.3424, "Lahore 3 Swimming Pool"
-                  ),
-                ),
-  
-
-             
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes("assets/swim3.jpg", 31.4756, 74.3424,
+                  "Lahore 3 Swimming Pool"),
+            ),
+          ],
+        ),
       ),
-
-      
-    ),
-  
-  );
-}
-
+    );
+  }
 
 // StyledBox(width:10.0),
 //   TextButton(
@@ -122,21 +105,50 @@ Widget _buildContainer() {
 //   onPressed: () { },
 //   child: Text('Confirm Location'),
 
+  Widget _boxes(String _image, double lat, double long, String resutantName) {
+    return GestureDetector(
+      onTap: () {
+        //GOES TO LOCATION ON MAP ON TAP
+        _gotolocation(lat, long);
+      },
+      child: Container(
+          child: new FittedBox(
+              child: Material(
+        color: Colors.white,
+        elevation: 14.0,
+        borderRadius: BorderRadius.circular(24.0),
 
-Widget _boxed(String _image, double lat,double long , String resutantName) {
-  return GestureDetector(
-    onTap: () {
-      //GOES TO LOCATION ON MAP ON TAP
-      _gotolocation(lat,long);
-    }
-  ),
-  child : Container(
-      child: new FittedBox (
-        child : Material()
-      )
-  ),
-}
+        shadowColor: Color(0x802196F3),
+        child : Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+                width:180,
+                height:200,
+                //clips own child
+                child:ClipRRect(
+                  borderRadius: new BorderRadius.circular(24.0),
 
+                  child: Image(
+                    fit:BoxFit.fill,
+                    image: NetworkImage(_image),
+
+                  ),
+                  ),
+            ),
+            Container ( 
+              child:Padding(
+                padding: const EdgeInsets.all(8.0),
+                // child: myDetailsContainer(resutantName), ?
+              ),
+              ), 
+          ]
+        ), 
+
+
+      ))),
+    );
+  }
 
 
 
